7b1aae46af5096c5e116e1357dd7e7ca49c0a06e
leo jaimes <leo777jaimes@gmail.com>
2024-11-28 12:16:40 -0600

    upgrade

---
+++ src/app/dbz/components/add-character-form/add-character-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character-form',
+  standalone: false,
+  
+  templateUrl: './add-character-form.component.html',
+  styleUrl: './add-character-form.component.scss'
+})
+export class AddCharacterFormComponent {
+
+}
---
+++ src/app/dbz/components/character-list/character-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-list',
+  standalone: false,
+
+  templateUrl: './character-list.component.html',
+  styleUrl: './character-list.component.scss',
+})
+export class CharacterListComponent {}
