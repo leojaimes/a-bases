bcb27cc2109189b1275118f49206651a39593f7c
leo jaimes <leo777jaimes@gmail.com>
2024-11-28 09:29:25 -0600

    upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'
 import { TodoListComponent } from './components/todolist/todolist.component';
 import { CounterComponent } from "./components/counter/counter.component";
+import { HeroComponent } from './components/heros/hero/hero.component';
+import { ListComponent } from './components/heros/list/list.component';
 
 
 
@@ -13,7 +15,9 @@ import { CounterComponent } from "./components/counter/counter.component";
   declarations: [
     AppComponent,
     TodoListComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
