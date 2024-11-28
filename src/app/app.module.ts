28f48de4d5de95df3771929e75c01c07648b58b8
leo jaimes <leo777jaimes@gmail.com>
2024-11-28 11:51:14 -0600

    upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { TodoListComponent } from './components/todolist/todolist.component';
-import { CounterComponent } from "./components/counter/counter.component";
+import { CounterComponent } from './components/counter/counter.component';
 import { HeroComponent } from './components/heros/hero/hero.component';
 import { ListComponent } from './components/heros/list/list.component';
-
-
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -17,15 +16,10 @@ import { ListComponent } from './components/heros/list/list.component';
     TodoListComponent,
     CounterComponent,
     HeroComponent,
-    ListComponent
+    ListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-
-],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, DbzModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  standalone: false,
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+  constructor() {}
+}
